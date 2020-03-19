92cb40e84cb9e9adc127abc4fb2fb024e0b5120a
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-18 21:33:45 -0400

    refactor javascript to utilize let and const instead of var

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 (function() {
-	var navbars = document.getElementsByTagName("nav");
-	for (var i = 0; i < navbars.length; i++) {
+	const navbars = document.getElementsByTagName("nav");
+	for (let i = 0; i < navbars.length; i++) {
 		(function(nav) {
-			var expanded = false;
-			var navList = nav.querySelector(".nav-list");
-			var navArrow = nav.querySelector(".navbar-expand-arrow");
+			let expanded = false;
+			const navList = nav.querySelector(".nav-list");
+			const naconstrow = nav.querySelector(".navbar-expand-arrow");
 			nav.querySelector(".navbar-expand").onclick = function() {
 				expanded = !expanded;
 				if (expanded) {
 					navList.style.display = "block";
 					setTimeout(function() {
-						var height = nav.querySelector(".nav-item").clientHeight * nav.querySelectorAll(".nav-item").length;
+						const height = nav.querySelector(".nav-item").clientHeight * nav.querySelectorAll(".nav-item").length;
 						navList.style.height = height + "px";
 					}, 0);
-					navArrow.innerText = "▲";
+					naconstrow.innerText = "▲";
 				} else {
 					navList.style.height = "0px";
-					navArrow.innerText = "▼";
+					naconstrow.innerText = "▼";
 				}
 			};
 			navList.addEventListener("transitionend", function() {
